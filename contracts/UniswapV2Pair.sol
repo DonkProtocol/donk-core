ec2365db57f63692b6ec43fd5868472572b88791
willcordeiro <willcordeiro800@gmail.com>
2023-07-01 20:16:05 -0300

    updating

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -260,8 +260,8 @@ contract UniswapV2Pair is UniswapV2ERC20 {
             balance1 = IERC20Uniswap(_token1).balanceOf(address(this));
         }
 
-        uint amount0In = balance0 > _reserve0 - amount1OutAfterFee ? balance0 - (_reserve0 - amount1OutAfterFee) : 0;
-        uint amount1In = balance1 > _reserve1 - amount0OutAfterFee ? balance1 - (_reserve1 - amount0OutAfterFee) : 0;
+        uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
+        uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
         require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
 
         {
