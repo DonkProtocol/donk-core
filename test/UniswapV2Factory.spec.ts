5c2c3f06a6b37bdbf1bfe29ba4f77f8b42d1e73e
willcordeiro <willcordeiro800@gmail.com>
2023-07-02 01:39:08 -0300

    up

--- test/UniswapV2Factory.spec.ts
+++ test/UniswapV2Factory.spec.ts
@@ -68,7 +68,7 @@ describe('UniswapV2Factory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2955923)
+    expect(receipt.gasUsed).to.eq(2836541)
   })
 
   it('setFeeTo', async () => {
